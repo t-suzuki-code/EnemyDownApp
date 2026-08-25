39d1f0fb630aa6e8c17eeda8d6f463e8b8cb04eb
t-suzuki-code <tomotomoj0914@icloud.com>
2026-08-25 20:57:42 +0900

    Initial commit

---
+++ src/main/java/plugin/enemydown/app/mapper/data/GameConfig.java
@@ -0,0 +1,16 @@
+package plugin.enemydown.app.mapper.data;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+
+public class GameConfig {
+
+  private int id;
+  private int gameTime;
+  private String difficulty;
+}
